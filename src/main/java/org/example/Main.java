5548d10215333a891a7ef34aa591f6fdb522fddd
FrenzyZ7 <rubenverrod@gmail.com>
2023-09-28 13:46:55 +0200

    necessary commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,7 @@
 package org.example;
+import java.io.File;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
@@ -9,6 +12,12 @@ public class Main {
 
     }
     public static void placeholderName(File givenFile){
+
+        /*Dividir fichero en varios ficheros en funcion de
+        numero dado de caracteres, y numero dado de lineas,
+        la segunda parte del ejercicio pide que; dada una lista de ficheros,
+        se hará un fichero que será la suma de los anteriores*/
+
 
     }
 }
